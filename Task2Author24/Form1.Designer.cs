79f3f45259d5a3013b9de507f71cc15bdfbd6946
azedefender <166021791+azedefender@users.noreply.github.com>
2024-12-10 09:34:46 +0300

    Приложение WindowsForms
    Полный код приложения Windows Forms для варианта 42

---
+++ Task2Author24/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Task2Author24
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(284, 48);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.Size = new System.Drawing.Size(199, 105);
+            this.textBoxOutput.TabIndex = 0;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(315, 159);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(138, 33);
+            this.buttonCalculate.TabIndex = 1;
+            this.buttonCalculate.Text = "Вычислить";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Исходная матрица (Результат в этом же поле)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 230);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(693, 121);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxOutput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Task2Author24/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2Author24
+{
+    public partial class Form1 : Form
+    {
+
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            int n = 7; // длина кода
+            int d = 4; // минимальное расстояние
+            List<string> codeWords = GenerateCodeWords(n, d);
+
+            // Выводим результаты
+            textBoxOutput.Clear();
+            foreach (var word in codeWords)
+            {
+                textBoxOutput.AppendText(word + "\n");
+            }
+        }
+
+        private List<string> GenerateCodeWords(int n, int d)
+        {
+            List<string> codeWords = new List<string>();
+            int totalWords = (int)Math.Pow(2, n);
+
+            for (int i = 0; i < totalWords; i++)
+            {
+                string binaryString = Convert.ToString(i, 2).PadLeft(n, '0');
+                if (IsValidCodeWord(binaryString, codeWords, d))
+                {
+                    codeWords.Add(binaryString);
+                }
+            }
+
+            return codeWords;
+        }
+
+        private bool IsValidCodeWord(string newWord, List<string> existingWords, int d)
+        {
+            foreach (var word in existingWords)
+            {
+                if (HammingDistance(newWord, word) < d)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private int HammingDistance(string str1, string str2)
+        {
+            int distance = 0;
+            for (int i = 0; i < str1.Length; i++)
+            {
+                if (str1[i] != str2[i])
+                {
+                    distance++;
+                }
+            }
+            return distance;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
